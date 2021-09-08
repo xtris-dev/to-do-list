3d7ad9aaa679c1f4090359a8499032c1d6e4deb7
Tristan Bouchet <88005969+Tristanbct@users.noreply.github.com>
2021-09-08 18:16:20 +0200

    Fix remove bug

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const writeTask = document.getElementById("text");
 const addTask = document.getElementById("submit");
-let todo = document.getElementById("todo");
+const todo = document.getElementById("todo");
 let task = "";
 let newTask;
 let removeTask;
-
 
 // Put input text content into a new variable
 
@@ -12,20 +11,26 @@ writeTask.addEventListener("input", (e) => {
     task = e.target.value;
 });
 
-
 // Check if input variable isn't empty. If okay, insert a new div with input text value
 
 function addTheTask() {
+    const taskID = Math.random().toString().substring(4, 10);
     newTask = document.createElement("div");
     newTask.classList.add("task");
+    newTask.setAttribute("ID", taskID);
     newTask.innerHTML = `
         <p>${task}</p>
-        <input type="button" value="Remove" id="remove" onclick="removeTheTask()">`
+        <input type="button" value="Remove" id="remove" onclick="removeTheTask('${taskID}')">`
     todo.appendChild(newTask);
 }
 
-function removeTheTask() {
-    newTask.remove();
+function removeTheTask(taskID) {
+    let tasks = document.getElementsByClassName("task");
+    for (let i = 0; i < tasks.length; i++) {
+        if (tasks[i].id === taskID) {
+            tasks[i].remove();
+        }
+    }
 }
 
 addTask.addEventListener("click", (e) => {
